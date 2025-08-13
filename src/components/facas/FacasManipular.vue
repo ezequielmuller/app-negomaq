bfb7e2402672bf4d80dc57296108828a3c40d305
Ezequiel <zikimuller017@gmail.com>
2025-08-13 19:24:55 -0300

    perf: adicionado try-catch nas chamadas da api para tratamento dos erros

--- src/components/facas/FacasManipular.vue
+++ src/components/facas/FacasManipular.vue
@@ -1,90 +1,90 @@
 <template>
-    <q-dialog v-model="dialogGravar" persistent>
-        <q-card style="width: 400px; max-height: 50vh" class="column no-wrap">
-            <q-card-section class="bg-primary text-white">
-                <div class="row items-center" style="gap: 8px">
-                    <q-icon name="add" size="md" />
-                    <div class="text-h6">Adicionar Faca</div>
-                </div>
-            </q-card-section>
+  <q-dialog v-model="dialogGravar" persistent>
+    <q-card style="width: 400px; max-height: 50vh" class="column no-wrap">
+      <q-card-section class="bg-primary text-white">
+        <div class="row items-center" style="gap: 8px">
+          <q-icon name="add" size="md" />
+          <div class="text-h6">Adicionar Faca</div>
+        </div>
+      </q-card-section>
 
-            <q-card-section>
-                <div class="row q-col-gutter-sm">
-                    <div class="col-12">
-                        <q-input dense flat outlined v-model="nome" label="Nome do Produto" />
-                    </div>
-                    <div class="col-12">
-                        <q-input dense flat outlined v-model="descricao" label="Descrição" type="textarea" />
-                    </div>
-                    <div class="col-6">
-                        <q-input dense flat outlined v-model="preco" label="Preço do Produto" />
-                    </div>
-                    <div class="col-6">
-                        <q-select dense flat outlined v-model="categoria" :options="categorias" option-label="label"
-                            option-value="value" emit-value map-options />
+      <q-card-section>
+        <div class="row q-col-gutter-sm">
+          <div class="col-12">
+            <q-input dense flat outlined v-model="nome" label="Nome do Produto" />
+          </div>
+          <div class="col-12">
+            <q-input dense flat outlined v-model="descricao" label="Descrição" type="textarea" />
+          </div>
+          <div class="col-6">
+            <q-input dense flat outlined v-model="preco" label="Preço do Produto" />
+          </div>
+          <div class="col-6">
+            <q-select dense flat outlined v-model="categoria" :options="categorias" option-label="label"
+              option-value="value" emit-value map-options label="Categoria" />
 
-                    </div>
-                    <div class="col-12">
-                        <q-file v-model="img" label="Imagem" multiple accept="image/*" outlined flat dense>
-                            <template v-slot:prepend>
-                                <q-icon name="attach_file" />
-                            </template>
-                        </q-file>
-                    </div>
-                </div>
-            </q-card-section>
+          </div>
+          <div class="col-12">
+            <q-file v-model="img" label="Imagem" multiple accept="image/*" outlined flat dense>
+              <template v-slot:prepend>
+                <q-icon name="attach_file" />
+              </template>
+            </q-file>
+          </div>
+        </div>
+      </q-card-section>
 
-            <q-separator style="height: 3px;" class="bg-primary q-mr-md q-ml-md" />
+      <q-separator style="height: 3px;" class="bg-primary q-mr-md q-ml-md" />
 
-            <q-card-actions align="right" class="q-mr-sm q-mb-xs">
-                <q-btn outline label="Fechar" color="primary" v-close-popup />
-                <q-btn label="Gravar" color="primary" @click="gravarFaca" icon="save" />
-            </q-card-actions>
-        </q-card>
-    </q-dialog>
+      <q-card-actions align="right" class="q-mr-sm q-mb-xs">
+        <q-btn outline label="Fechar" color="primary" v-close-popup />
+        <q-btn label="Gravar" color="primary" @click="gravarFaca" icon="save" />
+      </q-card-actions>
+    </q-card>
+  </q-dialog>
 
-    <q-dialog v-model="dialogEditar" persistent>
-        <q-card style="width: 400px; max-height: 50vh" class="column no-wrap">
-            <q-card-section class="bg-primary text-white">
-                <div class="row items-center" style="gap: 8px">
-                    <q-icon name="edit_square" size="md" />
-                    <div class="text-h6">Alterar Faca</div>
-                </div>
-            </q-card-section>
+  <q-dialog v-model="dialogEditar" persistent>
+    <q-card style="width: 400px; max-height: 50vh" class="column no-wrap">
+      <q-card-section class="bg-primary text-white">
+        <div class="row items-center" style="gap: 8px">
+          <q-icon name="edit_square" size="md" />
+          <div class="text-h6">Alterar Faca</div>
+        </div>
+      </q-card-section>
 
-            <q-card-section>
-                <div class="row q-col-gutter-sm">
-                    <div class="col-12">
-                        <q-input dense flat outlined v-model="nomeEditado" label="Nome do Produto" />
-                    </div>
-                    <div class="col-12">
-                        <q-input dense flat outlined v-model="descricaoEditado" label="Descrição" type="textarea" />
-                    </div>
-                    <div class="col-6">
-                        <q-input dense flat outlined v-model="precoEditado" label="Preço do Produto" />
-                    </div>
-                    <div class="col-6">
-                        <q-select dense flat outlined v-model="categoriaEditado" :options="categorias"
-                            option-label="label" option-value="value" />
-                    </div>
-                    <div class="col-12">
-                        <q-file v-model="imgEditado" label="Imagem" multiple accept="image/*" outlined flat dense>
-                            <template v-slot:prepend>
-                                <q-icon name="attach_file" />
-                            </template>
-                        </q-file>
-                    </div>
-                </div>
-            </q-card-section>
+      <q-card-section>
+        <div class="row q-col-gutter-sm">
+          <div class="col-12">
+            <q-input dense flat outlined v-model="nomeEditado" label="Nome do Produto" />
+          </div>
+          <div class="col-12">
+            <q-input dense flat outlined v-model="descricaoEditado" label="Descrição" type="textarea" />
+          </div>
+          <div class="col-6">
+            <q-input dense flat outlined v-model="precoEditado" label="Preço do Produto" />
+          </div>
+          <div class="col-6">
+            <q-select dense flat outlined v-model="categoriaEditado" :options="categorias" option-label="label"
+              option-value="value" label="Categoria" />
+          </div>
+          <div class="col-12">
+            <q-file v-model="imgEditado" label="Imagem" multiple accept="image/*" outlined flat dense>
+              <template v-slot:prepend>
+                <q-icon name="attach_file" />
+              </template>
+            </q-file>
+          </div>
+        </div>
+      </q-card-section>
 
-            <q-separator style="height: 3px;" class="bg-primary q-mr-md q-ml-md" />
+      <q-separator style="height: 3px;" class="bg-primary q-mr-md q-ml-md" />
 
-            <q-card-actions align="right" class="q-mr-sm q-mb-xs">
-                <q-btn outline label="Fechar" color="primary" v-close-popup />
-                <q-btn label="Gravar" color="primary" @click="editarFaca" icon="save" />
-            </q-card-actions>
-        </q-card>
-    </q-dialog>
+      <q-card-actions align="right" class="q-mr-sm q-mb-xs">
+        <q-btn outline label="Fechar" color="primary" v-close-popup />
+        <q-btn label="Gravar" color="primary" @click="editarFaca" icon="save" />
+      </q-card-actions>
+    </q-card>
+  </q-dialog>
 </template>
 <script setup lang="ts">
 import { ref, watch } from 'vue'
@@ -100,16 +100,16 @@ const dialogEditar = ref(props.dialogEditar)
 const $q = useQuasar()
 
 watch(() => props.dialogGravar, val => {
-    dialogGravar.value = val
+  dialogGravar.value = val
 })
 watch(dialogGravar, val => {
-    emit('update:dialogGravar', val)
+  emit('update:dialogGravar', val)
 })
 watch(() => props.dialogEditar, val => {
-    dialogEditar.value = val
+  dialogEditar.value = val
 })
 watch(dialogEditar, val => {
-    emit('update:dialogEditar', val)
+  emit('update:dialogEditar', val)
 })
 
 
@@ -127,42 +127,47 @@ const imgEditado = ref(null)
 const produtoId = ref<number | null>(null)
 
 const categorias = [
-    { label: 'Facas', value: 'facas' },
-    { label: 'Estojos', value: 'estojos' },
-    { label: 'Aventais', value: 'aventais' },
-    { label: 'Churrascos', value: 'churrascos' }
+  { label: 'Facas', value: 'facas' },
+  { label: 'Estojos', value: 'estojos' },
+  { label: 'Aventais', value: 'aventais' },
+  { label: 'Churrascos', value: 'churrascos' }
 ]
 
 const gravarFaca = async () => {
-    try {
-        $q.loading.show({ message: 'Cadastrando Produto...' })
-        const data = { nome: nome.value, descricao: descricao.value, preco: Number(preco.value), categoria: categoria.value }
-        await criarProduto(data)
-        $q.notify({ type: 'positive', message: 'Produto criado!' })
-        dialogGravar.value = false
-    } catch {
-        $q.notify({ type: 'negative', message: 'Erro ao editar produto!' })
-    } finally {
-        $q.loading.hide()
+  try {
+    $q.loading.show({ message: 'Cadastrando Produto...' })
+    const data = {
+      nome: nome.value,
+      descricao: descricao.value,
+      preco: Number(preco.value),
+      categoria: categoria.value
     }
+    await criarProduto(data)
+    $q.notify({ type: 'positive', message: 'Produto criado!' })
+    dialogGravar.value = false
+  } catch {
+    $q.notify({ type: 'negative', message: 'Erro ao editar produto!' })
+  } finally {
+    $q.loading.hide()
+  }
 }
 
 const editarFaca = async () => {
-    try {
-        $q.loading.show({ message: 'Alterando Produto...' })
-        const data = { nome: nomeEditado.value, descricao: descricaoEditado.value, preco: Number(precoEditado.value), categoria: categoriaEditado.value }
-        if (produtoId.value !== null) {
-            await atualizarProduto(String(produtoId.value), data)
-        } else {
-            throw new Error('ID do produto não definido')
-        }
-        $q.notify({ type: 'positive', message: 'Produto atualizado!' })
-        dialogEditar.value = false
-    } catch {
-        $q.notify({ type: 'negative', message: 'Erro ao editar produto!' })
-    } finally {
-        $q.loading.hide()
+  try {
+    $q.loading.show({ message: 'Alterando Produto...' })
+    const data = { nome: nomeEditado.value, descricao: descricaoEditado.value, preco: Number(precoEditado.value), categoria: categoriaEditado.value }
+    if (produtoId.value !== null) {
+      await atualizarProduto(String(produtoId.value), data)
+    } else {
+      throw new Error('ID do produto não definido')
     }
+    $q.notify({ type: 'positive', message: 'Produto atualizado!' })
+    dialogEditar.value = false
+  } catch {
+    $q.notify({ type: 'negative', message: 'Erro ao editar produto!' })
+  } finally {
+    $q.loading.hide()
+  }
 }
 
 </script>
--- src/services/produtoService.ts
+++ src/services/produtoService.ts
@@ -3,34 +3,54 @@ import type { ProdutoPayload } from '../types/types';
 
 const VITE_ADMIN_TOKEN = import.meta.env.VITE_ADMIN_TOKEN;
 
-export const criarProduto = (produto: ProdutoPayload) =>
-  api.post('/admin/produtos', produto, {
-    headers: {
-      Authorization: VITE_ADMIN_TOKEN,
-    },
-  });
-
-export const deletarProduto = (id: string) =>
-  api.delete(`/admin/produtos/${id}`, {
-    headers: {
-      Authorization: VITE_ADMIN_TOKEN,
-    },
-  });
-
-export const atualizarEstoque = (id: string, estoque: number) =>
-  api.put(
-    `/admin/estoque/${id}`,
-    { estoque },
-    {
+export const criarProduto = async (produto: ProdutoPayload) => {
+  try {
+    await api.post('/admin/produtos', produto, {
       headers: {
         Authorization: VITE_ADMIN_TOKEN,
       },
-    },
-  );
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-export const atualizarProduto = (id: string, dados: ProdutoPayload) =>
-  api.put(`/admin/produtos/${id}`, dados, {
-    headers: {
-      Authorization: VITE_ADMIN_TOKEN,
-    },
-  });
+export const deletarProduto = async (id: string) => {
+  try {
+    await api.delete(`/admin/produtos/${id}`, {
+      headers: {
+        Authorization: VITE_ADMIN_TOKEN,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const atualizarEstoque = async (id: string, estoque: number) => {
+  try {
+    await api.put(
+      `/admin/estoque/${id}`,
+      { estoque },
+      {
+        headers: {
+          Authorization: VITE_ADMIN_TOKEN,
+        },
+      },
+    );
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const atualizarProduto = async (id: string, dados: ProdutoPayload) => {
+  try {
+    await api.put(`/admin/produtos/${id}`, dados, {
+      headers: {
+        Authorization: VITE_ADMIN_TOKEN,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
