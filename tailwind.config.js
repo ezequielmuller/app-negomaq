e68c4c2179f360f425fae4e9c68854617d647a68
Ezequiel <zikimuller017@gmail.com>
2025-08-13 20:11:30 -0300

    style:Adicionado tailwind 4 no projeto

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{vue,js,ts,jsx,tsx}', // observa todos os arquivos do Quasar
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
