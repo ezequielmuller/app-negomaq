5f12498c736395173a44b6ea5d268c5f51ba9dd2
Ezequiel <zikimuller017@gmail.com>
2025-11-04 19:38:31 -0300

    style(layout-login): ajuste footer login e cadastro

--- src/layouts/LoginLayout.vue
+++ src/layouts/LoginLayout.vue
@@ -9,12 +9,12 @@
       </q-toolbar>
     </q-header>
 
-    <q-page-container style="height: 500px;">
+    <q-page-container>
       <router-view />
     </q-page-container>
-    <q-footer class="bg-black text-grey" height-hint="60">
+    <q-footer class="bg-black text-grey q-pa-sm" height-hint="64">
       <div class="row q-col-gutter-md justify-center items-center text-center">
-        <div class="col-12 flex justify-center items-center q-pt-sm" style="font-size: 0.7rem;">
+        <div class="col-12 flex justify-center items-center q-pt-md" style="font-size: 0.7rem;">
           <a href="https://api.whatsapp.com/send/?phone=5554996641291&text&type=phone_number&app_absent=0"
             target="_blank" class="q-mx-sm">
             <i class="fab fa-whatsapp fa-2x" style="color: #25D366;"></i>
@@ -23,7 +23,7 @@
             <i class="fab fa-instagram fa-2x" style="color: #C13584;"></i>
           </a>
         </div>
-        <div class="col-12 flex justify-center items-center q-pt-xs" style="font-size: 0.7rem; padding-bottom: 0.6rem;">
+        <div class="col-12 flex justify-center items-center q-pt-xs" style="font-size: 0.7rem;">
           <q-icon name="copyright" />
           <span class="q-ml-xs">
             2025 Todos os direitos reservados - NegoMaq Couros
